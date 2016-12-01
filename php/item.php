2d2dda6b6c9613c1752dab0cf00f8d94b67d5b13
Unknown <windisch33@gmail.com>
2016-12-01 10:37:58 -0500

    Deleted some junk

--- php/item.php
+++ php/item.php
@@ -4,21 +4,13 @@ require_once("db.php")
 function getAllItems(){
 
 	//get all current item in the database
-	$conn = get_MySQLi_Connection();
-	$stmt = "SELECT * FROM item";
-	$result = $conn->query($stmt);
-	$rows = $result->num_rows;
+
 	//return $result;
 }
 
 function getItemByID($id){
 
 	//return the specific item, with this specific ID.
-	// $conn = get_MySQLi_Connection();
-	// $query = sprintf("SELECT name, price, image  FROM item
-  //   WHERE itemId='%i'",
-  //   mysql_real_escape_string($id);
-	//
-	// $result = $conn->query($query);
+
 
 }
