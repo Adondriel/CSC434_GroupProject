f58d82e8ac65a37df1e949b50303ffa9959186e5
David Burkett <db0349@cs.ship.edu>
2016-12-04 16:05:58 -0500

    committing

--- php/checkout.php
+++ php/checkout.php
@@ -1,10 +1,48 @@
 <?php 
-
     if(isset($_SESSION['userId']) && $_SESSION['userId'])
     {
-        if(isset($_POST["inputFirstName"]))
+        //Shipping Info
+        if(isset($_POST["inputFirstName"]) )
         {
-            echo "Something";
+            $inputFirstName = FILTER_SANITIZE_STRING($_POST["inputFirstName"]);
+        }
+        if(isset($_POST["inputLastName"]) )
+        {
+            $inputLastName = FILTER_SANITIZE_STRING($_POST["inputLastName"]);
+        }
+        if(isset($_POST["inputAddressLine1"]) )
+        {
+            $inputAddressLine1 = $_POST["inputAddressLine1"];
+        }
+        if(isset($_POST["inputAddressLine2"]) )
+        {
+            $inputAddressLine2 = $_POST["inputAddressLine2"];
+        }
+        if(isset($_POST["inputCity"]) )
+        {
+            $inputAddressLine2 = $_POST["inputCity"];
+        }
+        if(isset($_POST["inputState"]) )
+        {
+            $inputState = $_POST["inputState"];
+        }
+        if(isset($_POST["inputCountry"]) )
+        {
+            $inputCountry = $_POST["inputCountry"];
+        }
+
+        //Billing info
+        if(isset($_POST["inputBillingName"]) )
+        {
+            $inputBillingName = $_POST["inputBillingName"];
+        }
+        if(isset($_POST["inputCardNumber"]) )
+        {
+            $inputCardNumber = $_POST["inputCardNumber"];
+        }
+        if(isset($_POST["inputSecurityCode"]) )
+        {
+            $inputSecurityCode = $_POST["inputSecurityCode"];
         }
     }
 ?>
