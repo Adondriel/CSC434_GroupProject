cdb872d5809c58e8dbc1bcd35af820eed4c7e3dc
Adam Pine <adondrielcraft@gmail.com>
2016-12-07 00:05:33 -0500

    Wishlist frontend changes

--- js/wishlist.controller.js
+++ js/wishlist.controller.js
@@ -1,16 +1,30 @@
-routerApp.controller('wishlistController', function($scope) {  
-    $scope.featuredItems = [
-        {
-            name: 'Macallan 12',
-            price: 50
-        },
-        {
-            name: 'Chivas Regal Royal Salute',
-            price: 10000
-        },
-        {
-            name: 'Glenfiddich 1937',
-            price: 20000
-        }
-    ];    
+routerApp.controller('wishlistController', function ($scope) {
+
+    $scope.addItemToWishlist = function (itemId) {
+        //this will send the itemId to the php file, with the itemId that needs to be added.
+        $.ajax({
+            url: '',
+            type: 'POST',
+            data: { func: "addItemToWishlist", itemId: itemId },
+            success: function (response) {
+                console.info("Added to wishlist");
+                //alert(response); // do what you like with the response
+                //alert("Thank you!");
+                //localStorage.removeItem('cart');
+            }
+        });
+    }
+    $scope.removeItemFromWishlist = function(itemId){
+        $.ajax({
+            url: '',
+            type: 'POST',
+            data: { func: "removeItemFromWishlist", itemId: itemId },
+            success: function (response) {
+                console.info("removedFromWithlist")
+                //alert(response); // do what you like with the response
+                //alert("Thank you!");
+                //localStorage.removeItem('cart');
+            }
+        });
+    }
 });
